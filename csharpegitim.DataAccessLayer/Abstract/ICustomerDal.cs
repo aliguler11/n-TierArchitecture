c4608c8d5b5c6c3cb129bd4fcba193d72d9372ff
aliguler11 <aliguler9@hotmail.com>
2026-02-19 16:16:55 +0300

    Add project files.

---
+++ csharpegitim.DataAccessLayer/Abstract/ICustomerDal.cs
@@ -0,0 +1,15 @@
+﻿using csharpegitim.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpegitim.DataAccessLayer.Abstract
+{
+    public interface ICustomerDal:IGenericDal<Customer>
+    {
+
+
+    }
+}
---
+++ csharpegitim.DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using csharpegitim.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpegitim.DataAccessLayer.Abstract
+{
+    public interface IProductDal : IGenericDal<Product>
+
+    {
+
+
+
+    }
+}
